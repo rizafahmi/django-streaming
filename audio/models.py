feba3151147392e89da779d523255526d213ccd5
rizafahmi <rizafahmi@gmail.com>
2011-10-27 22:16:06 +0700

    Update audio/models.py

--- audio/models.py
+++ audio/models.py
@@ -3,7 +3,7 @@ import datetime
 
 class audio(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    audio_file = models.FileField(upload_to='media/uplados', null=False)
+    audio_file = models.FileField(upload_to='media/uploads', null=False)
     genre = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     upload_date = models.DateTimeField(default=datetime.datetime.now)
